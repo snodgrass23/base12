332b229938cb19c3bc42c12e86a89d4ed2d8a343
Hunter Loftis <hunter.loftis+github@gmail.com>
2011-03-24 15:31:11 -0400

    Cleaned up demo

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,7 +1,7 @@
 exports = module.exports = {
 
   index: function(req, res, next) {
-    res.status(500, 'error', 'Something broke');
+    res.status(200, 'info', 'This is a message');
     res.context({ message: "Hello, world!" }, 'home/index', req.param('format'));
   }
   
