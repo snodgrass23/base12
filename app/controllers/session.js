c089cbda5d8df613792ac68ef1d49a751ba6fcd7
Hunter Loftis <hunter+github@hunterloftis.com>
2012-01-07 22:24:27 -0500

    Simplified session.create

--- app/controllers/session.js
+++ app/controllers/session.js
@@ -18,23 +18,10 @@ module.exports = {
 
   // Login POST
   // TODO: Check referrer to prevent login attacks
-  create: function(req, res) {
-    console.log("========");
-    passport.authenticate('local', function(err, user) {
-      console.log("Authentication complete...");
-      if (!err && user) {
-        console.log("User found, logging in...");
-        return req.login(user, function(err) {
-          console.log("Done.");
-          res.redirect(server.dynamicViewHelpers.account_route(req));
-        });
-      }
-      else {
-        console.log("User not found", err, user);
-        res.redirect('/login');
-      }
-    })(req, res);
-  },
+  create: [
+    passport.authenticate('local', { failureRedirect: '/sessions/login' }),
+    function (req, res) { res.redirect('/'); }
+  ],
 
   // Logout
   destroy: function(req, res) {
