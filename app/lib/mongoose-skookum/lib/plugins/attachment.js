172b92acf354ddb03a93c52688e1c322874251e4
Hunter Loftis <hunter+github@hunterloftis.com>
2012-01-14 23:12:48 -0500

    Good start with attachment plugin, potential security risk where someone could set the 'photo_flag' property?

--- app/lib/mongoose-skookum/lib/plugins/attachment.js
+++ app/lib/mongoose-skookum/lib/plugins/attachment.js
@@ -1,13 +1,6 @@
 var fs = require('fs');
 var path = require('path');
 
-var attachment_props = {
-  path      : { type: String }, // Path to the saved file, including filename
-  filename  : { type: String }, // Original filename of upload
-  filetype  : { type: String }, // MIME type
-  size      : { type: Number }  // Filesize
-};
-
 var handlers = {
   'default': function(file, options, callback) {
     console.log("Handling FILE:", file);
@@ -30,33 +23,86 @@ var handlers = {
   }
 };
 
-module.exports = function AttachmentPlugin (schema, options) {
-  options = options || {};
-  options.property = options.property || false;
-  options.handler = options.handler || 'default';
-  options.schema = schema;
+var mongoose = require('mongoose');
+var ObjectId = mongoose.Schema.ObjectId;
 
-  schema.add(attachment_props, options.property + '.');
+var AttachmentSchema = new mongoose.Schema({
+  path: { type: String },
+  filename: { type: String},
+  filetype: { type: String },
+  size: { type: Number }
+}, {strict: true});
 
+function AttachmentPlugin(schema, options) {
+
+  // Create a new model (collection) to store these attachments
+  mongoose.model(options.ref, AttachmentSchema);
+
+  var property = options.property; // get: returns dbrefprop's value, set: updates dbrefprop/flagprop
+  var flagprop = options.property + '_flag'; // flags new attachments for processing
+  var dbrefprop = options.property + '_dbref'; // stores our attachment ObjectId (dbref), or null when we have no attachment
+
+  var properties = {};
+  properties[flagprop] = { type: String };
+  properties[dbrefprop] = { type: ObjectId, ref: options.ref };
+
+  // Add our properties to the schema
+  schema.add(properties);
+
+  // Intercept saves to check for new attachments to process
   schema.pre('save', function(next) {
-    var is_modified;
-
-    if (options.property) {
-      is_modified = this.isModified(options.property);
+    if (this[flagprop]) {
+      console.log("I should be processing the image at " + this[flagprop]);
     }
-    else {
-      is_modified = !!Object.keys(this._activePaths.states.modify).length;
-    }
-
-    console.log("IS MODIFIED? ", is_modified);
-    if (is_modified) {
-      var file_handler = handlers[options.handler];
-      var file_data = options.property ? this.toObject()[options.property] : this.toObject();
-
-      file_handler(file_data, options, function(err, res) {
-        return next(err);
-      });
-    }
-    else return next();
+    return next(new Error('Not saving to avoid users'));
   });
+
+  console.log("Setting up virtuals on " + property);
+  schema.virtual(property)
+    .get(function() {
+      console.log("virtual getter");
+      // property always returns the dbref
+      return this[dbrefprop];
+    })
+    .set(function(file) {
+      console.log("SETTER:", file);
+      // file = a path ('/tmp/1234') an Object ({ ... }) or an ObjectId (string or obj)
+      if (file instanceof ObjectId) {
+        // ObjectId
+        // file already in the DB, must be attached to the '_property' dbref
+        this[dbrefprop] = file;
+        return this[flagprop] = null;
+      }
+      else if (typeof(file) === 'string') {
+        // path or ObjectId string
+        if (~file.indexOf('/')) {
+          // path
+          return this[flagprop] = file;
+        }
+        else if (file.length === '12') {
+          // hexstring, mongoose will cast
+          this[dbrefprop] = file;
+          return this[flagprop] = null;
+        }
+      }
+      else if (typeof(file) === 'object' && file._id) {
+        // An object that has already been saved
+        this[dbrefprop] = file._id;
+        return this[flagprop] = null;
+      }
+    });
+}
+
+AttachmentPlugin.move = function(src, options, next) {
+  if (src.destination) {
+    // move file from src.path to src.destination
+  }
 };
+
+AttachmentPlugin.image = function(src, options, next) {
+  var filename = 'get_from_src.path' + '.jpg';
+  src.destination = base + '/uploads/' + filename;
+  return next();
+};
+
+module.exports = AttachmentPlugin;
--- app/models/user.js
+++ app/models/user.js
@@ -17,9 +17,72 @@ var User = new server.mongoose.Schema({
 User.plugin(skookum.plugins.password, { required: true });
 User.plugin(skookum.plugins.timestamps);
 User.plugin(skookum.plugins.crud);
+User.plugin(skookum.plugins.attachment, {
+  property: 'photo',
+  ref: 'UserPhoto',
+  preprocess: skookum.plugins.attachment.image,
+  width: 300
+});
 
 // Export
 
-module.exports = server.mongoose.model('User', User);
+var UserModel = module.exports = server.mongoose.model('User', User);
+
+console.log("CREATING USER");
+var new_user = new UserModel({
+  email: 'huntertestingattachments3@skookum.com',
+  name: 'Hunter Loftis',
+  password: 'password',
+  photo: '/tmp/12345'
+});
+new_user.save(function(err, doc) {
+  console.log("err:", err);
+  console.log("doc:", doc);
+});
+
+/*
+// Will be saved like:
 
 
+// Scenario 1: user fills out a form with his info + a file input, all in the same POST (vanilla):
+var new_user2 = new models.user({
+  email: 'hunter@skookum.com',
+  photo: '/tmp/12345'
+});
+
+// Scenario 2: user uploads a photo first via form A (maybe an ajax upload) which returns the saved photo JSON,
+// then saves his profile via form B which attaches the returned photo JSON to the POST.
+var new_photo2 = new models.user.photo({ path: '/tmp/12345' });
+new_photo2.save();
+// ...
+new_user.set({
+  name: 'Hunter Loftis',
+  photo: req.body.photo
+});
+new_user.save();
+
+// Scenario 3: user uploads a photo first via form A which returns the saved _id,
+// then saves his profile via form B which attaches the returned photo _id to the POST
+var new_photo3 = models.user.photo({ path: '/tmp/12345' });
+new_photo3.save();
+// ...
+new_user.set({
+  photo: photo_id
+});
+new_user.save();
+
+// Scenario 4: system provides user with a default photo by searching for the currently active default,
+// then attaches it via its ObjectId
+models.user.photo.find({ 'default': true }, function(err, default_photo) {
+  new_user.set({
+    photo: default_photo
+  });
+  new_user.save();
+});
+
+// Will be read like:
+
+models.user.find(match).populate('photo').run(function(err, users) {
+  
+});
+*/
