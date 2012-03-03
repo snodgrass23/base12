2e135f8cdf8340feb175c4823706188679e78575
Hunter Loftis <hunter+github@hunterloftis.com>
2012-03-03 13:56:20 -0500

    Updated postinstall and lock scripts

---
+++ scripts/postinstall.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+var path = require('path'),
+    fs = require('fs'),
+    util = require('util');
+
+var src = __dirname + '/../env/default.env.js',
+    dst = __dirname + '/../.env.js';
+
+path.exists(dst, function(exists) {
+  if (exists) {
+    console.log('Environment config (.env.js) found.');
+  }
+  else {
+    console.log('Environment config (.env.js) not found, copying from env/default.env.js...');
+    var rs = fs.createReadStream(src);
+    var ws = fs.createWriteStream(dst);
+    util.pump(rs, ws, function(err, result) {
+      if (err) throw err;
+      console.log('done.');
+    });
+  }
+});
