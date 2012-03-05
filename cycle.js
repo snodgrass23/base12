551c50bf272780cbfab3e15ec28a248deba651b3
Jim Snodgrass <jim@skookum.com>
2012-03-05 10:16:38 -0500

    remove old reload library that has been moved to server module

--- cycle.js
+++ cycle.js
@@ -1,10 +1,6 @@
 var base12 = require('base12');
 
-base12.balance(__dirname + '/app', process.argv[2]);
-
-// setup reloader on file change
-// this will kill process whenever a file is changed allowing cluster to fork a replacement
-require('./lib/reload')({
-  dir:__dirname+'/app',
+base12.cycle(__dirname + '/app', process.argv[2], {
   ignore: ['public']
 });
+
