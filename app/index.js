24337268de8c065d438399ceac0a8f8d25d80c82
Hunter Loftis <hunter+github@hunterloftis.com>
2012-03-07 09:17:36 -0500

    added autoload to libs

--- app/index.js
+++ app/index.js
@@ -1,10 +1,3 @@
 var base12 = require('base12');
 
-var locals = require('./lib/locals');
-var middleware = require('./lib/middleware');
-var routes = require('./lib/routes');
-
-module.exports = base12.app({
-  dir: __dirname,                             // Load Models, Views, Middleware, Controllers
-  init: [locals, middleware, routes]          // Steps to initialize your app
-});
+module.exports = base12.app(__dirname);
