0fce78053706d61a7f914cb9889744e2ca160bdf
Hunter Loftis <hunter@hunterloftis.com>
2012-03-27 11:03:53 -0400

    Removed custom connect.timeout branch, replaced with latest connect and connect-timeout.

--- app/lib/middleware.js
+++ app/lib/middleware.js
@@ -1,7 +1,8 @@
 var express         = require('express'),
     connect         = require('connect'),
     stylus          = require('stylus'),
-    RedisStore      = require('connect-redis')(express);
+    RedisStore      = require('connect-redis')(express),
+    connect_timeout = require('connect-timeout');
 
 // Middleware
 
@@ -28,12 +29,6 @@ module.exports = function(app) {
     maxAge: app.config.session_length
   });
 
-  // Timeouts
-  var timeout_middleware = connect.timeout({
-    throwError: true,
-    time: app.constants.request_timeout
-  });
-
   // Error handler
   var error_middleware = express.errorHandler({
     dumpExceptions: true,
@@ -41,15 +36,15 @@ module.exports = function(app) {
   });
 
   // Middleware stack for all requests
-  app.use(timeout_middleware);                              // request timeouts
-  app.use(express.compress());                              // gzip
-  app.use(stylus_middleware);                               // Compile .styl to .css
-  app.use(express.cookieParser(app.config.cookie_secret));  // req.cookies
-  app.use(session_middleware);                              // req.session
-  app.use(express.bodyParser());                            // req.body & req.files
-  app.use(express.methodOverride());                        // '_method' property in body (POST -> DELETE / PUT)
-  app.use(app.router);                                      // routes in lib/routes.js
-  app.use(express['static'](app.set('public')));            // static files in /public
+  app.use(connect_timeout({ time: app.constants.request_timeout }));  // request timeouts
+  app.use(express.compress());                                        // gzip
+  app.use(stylus_middleware);                                         // Compile .styl to .css
+  app.use(express.cookieParser(app.config.cookie_secret));            // req.cookies
+  app.use(session_middleware);                                        // req.session
+  app.use(express.bodyParser());                                      // req.body & req.files
+  app.use(express.methodOverride());                                  // '_method' property in body (POST -> DELETE / PUT)
+  app.use(app.router);                                                // routes in lib/routes.js
+  app.use(express['static'](app.set('public')));                      // static files in /public
   
   // Handle errors thrown from middleware/routes
   app.use(error_middleware);
