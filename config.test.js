8a83c705b1a26af79d1bd80f89a38dbb7c8ddd20
Jim Snodgrass <jim@skookum.com>
2013-01-03 10:42:45 -0500

    move config files to JS instead of JSON to allow for more flexibility and things like commenting

---
+++ config.test.js
@@ -0,0 +1,8 @@
+module.exports = {
+  http_port: 8000,
+  stylus_debug: false,
+  test: true,
+  cluster: false,
+  mongoose_url: "mongodb://localhost/base12_test",
+  session_secret: "base12secret_test"
+};
