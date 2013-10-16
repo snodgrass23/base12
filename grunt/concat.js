44dcd54a5df981c22d552a04f8cca69c4e60066f
Jim Snodgrass <jim@skookum.com>
2013-10-15 23:15:53 -0400

    lets add angular to base libs too

--- grunt/concat.js
+++ grunt/concat.js
@@ -3,7 +3,8 @@ module.exports = {
     files: [{
       src: [
         "public/scripts/components/jquery/jquery.min.js",
-        "public/scripts/components/lodash/dist/lodash.min.js"
+        "public/scripts/components/lodash/dist/lodash.min.js",
+        "public/scripts/components/angular-unstable/angular.min.js"
       ],
       dest: "public/scripts/compiled/components.js"
     }]
